a5cff7e3856e080e4bc47d405ba09c081edc349b
megha <kumarmegha54@gmail.com>
2026-03-04 12:25:36 +0530

    Added automatic model download from Huggingface

--- backend/inference/model_loader.py
+++ backend/inference/model_loader.py
@@ -1,15 +1,32 @@
+import os
 import torch
-from model.model_arch import create_model
+import timm
+import urllib.request
 
-CLASS_NAMES = ['Normal', 'Tuberculosis', 'covid', 'lung-opacity', 'pneumonia']
+MODEL_URL = "https://huggingface.co/Meghakumarr14/lung-disease-model/resolve/main/final_best_senet154.pth"
+MODEL_PATH = "model/final_best_senet154.pth"
+
+class_names = ['Normal', 'Tuberculosis', 'covid', 'lung-opacity', 'pneumonia']
+
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("Downloading model from HuggingFace...")
+        os.makedirs("model", exist_ok=True)
+        urllib.request.urlretrieve(MODEL_URL, MODEL_PATH)
+        print("Model downloaded successfully!")
 
 def load_model():
+    download_model()
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = create_model(num_classes=len(CLASS_NAMES))
-    model.load_state_dict(
-        torch.load("model/final_best_senet154.pth", map_location=device)
+    model = timm.create_model(
+        "senet154",
+        pretrained=False,
+        num_classes=len(class_names)
     )
+
+    model.load_state_dict(torch.load(MODEL_PATH, map_location=device))
     model.to(device)
     model.eval()
 
